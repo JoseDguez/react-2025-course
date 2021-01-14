ca64693488826eb571d6796862a7dbbf3b16b95b
Jose Antonio Dominguez Lemus <joseantonio.dguez@gmail.com>
2021-01-14 15:02:40 -0800

    Firestore to save user info and chakra-ui integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import '../styles/globals.css';
-import { ProvideAuth } from '../lib/auth';
+import { AuthProvider } from '../lib/auth';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
